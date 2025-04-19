149bacd96ac385b44ea027520de3496d12f481d8
Matteo <165385851+panmateuszbaran@users.noreply.github.com>
2025-04-20 00:42:50 +0200

    Update index.tsx

--- src/components/EventCalendar/index.tsx
+++ src/components/EventCalendar/index.tsx
@@ -1,4 +1,4 @@
-import { withTranslation, TFunction } from "react-i18next";
+import { withTranslation } from "react-i18next";
 import "../../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-base/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-calendars/styles/material.css";
